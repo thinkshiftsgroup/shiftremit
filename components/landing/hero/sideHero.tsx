6b703d573266ac3f91b9c991e0370fb548851491
honeybadger <joshviberg3@gmail.com>
2025-11-04 15:08:54 +0000

    feat(ui): update side hero layout and add login link to send button

--- components/landing/hero/sideHero.tsx
+++ components/landing/hero/sideHero.tsx
@@ -16,7 +16,7 @@ const SideHero = () => {
   const [receiveCurrency, setReceiveCurrency] = useState("BDT");
 
   return (
-    <div className="bg-main-dark-II w-[55%] rounded-xl p-6 sm:p-8 text-white shadow-xl">
+    <div className="w-[55%] flex items-start min-h-screen h-auto">
       {/* <h2 className="text-center font-semibold font-poppins text-lg mb-4">
         Get Started
       </h2> */}
@@ -41,26 +41,26 @@ const SideHero = () => {
           </p>
         </span>
       </div> */}
-      <Transfer />
+      <div className="bg-main-dark-II w-[90%] mx-auto rounded-xl p-6 sm:p-8 text-white shadow-xl mt-20">
+        <Transfer />
 
-      <div className="bg-[#ffffff0d] text-[#cccccc] rounded-lg p-4 mb-6 font-poppins text-sm space-y-2">
-        <div className="flex justify-between">
-          <span>Delivery Method</span>
-          <div className="*:text-white font-normal *:cursor-pointer flex items-center gap-1 text-sm ">
-            <p
-              onClick={() => setIsBank(true)}
-              className={`border relative ${
-                isBank ? "border-main" : "border-white"
-              } rounded-sm p-1`}
-            >
-              {isBank && (
-                <span className="bg-main text-white rounded-full inline-flex items-center justify-center w-3.5 h-3.5 absolute -top-2 -right-2">
-                  <IoIosCheckmark size={14} />
-                </span>
-              )}
-              Bank
-            </p>
-            {/* <p
+        <div className="bg-[#ffffff0d] text-[#cccccc] rounded-lg p-4 mb-6 font-poppins text-sm space-y-2">
+          <div className="flex justify-between">
+            <span>Delivery Method</span>
+            <div className="*:text-white font-normal *:cursor-pointer flex items-center gap-1 text-sm ">
+              <p
+                onClick={() => setIsBank(true)}
+                className={`border relative ${isBank ? "border-main" : "border-white"
+                  } rounded-sm p-1`}
+              >
+                {isBank && (
+                  <span className="bg-main text-white rounded-full inline-flex items-center justify-center w-3.5 h-3.5 absolute -top-2 -right-2">
+                    <IoIosCheckmark size={14} />
+                  </span>
+                )}
+                Bank
+              </p>
+              {/* <p
               onClick={() => setIsBank(false)}
               className={`border relative ${
                 !isBank ? "border-main" : "border-white"
@@ -73,40 +73,44 @@ const SideHero = () => {
               )}
               BKash
             </p> */}
+            </div>
+          </div>
+          <div className="flex font-poppins justify-between">
+            <span>Rate</span>
+            <span>1 GBP = 1,895.23 NGN</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Send Fee</span>
+            <span>0.00 GBP</span>
+          </div>
+          <div className="flex justify-between">
+            <span>Delivery Time</span>
+            <span>2 minutes</span>
           </div>
         </div>
-        <div className="flex font-poppins justify-between">
-          <span>Rate</span>
-          <span>1 GBP = 1,895.23 NGN</span>
-        </div>
-        <div className="flex justify-between">
-          <span>Send Fee</span>
-          <span>0.00 GBP</span>
-        </div>
-        <div className="flex justify-between">
-          <span>Delivery Time</span>
-          <span>2 minutes</span>
-        </div>
-      </div>
 
-      <div className="flex font-poppins justify-between items-center">
-        <div>
-          <p className="text-xs opacity-80">Total Amount</p>
-          <p className="font-semibold text-lg">1 GBP</p>
-        </div>
-        <button
-          className="
+        <div className="flex font-poppins justify-between items-center">
+          <div>
+            <p className="text-xs opacity-80">Total Amount</p>
+            <p className="font-semibold text-lg">1 GBP</p>
+          </div>
+          <a href="/login">
+            <button
+              className="
     text-base text-white font-poppins border border-[#813FD6] py-3 px-6 font-medium rounded-[6px] cursor-pointer
     bg-linear-to-l from-[#813FD6] to-[#301342]
     transition-all duration-300 ease-in-out
     hover:border-transparent flex items-center gap-2
   "
-        >
-          Send Now <CgArrowTopRight />
-        </button>
+            >
+              Send Now <CgArrowTopRight />
+            </button>
+            </a>
+        </div>
+
+        <CompareRates />
       </div>
 
-      <CompareRates />
     </div>
   );
 };
