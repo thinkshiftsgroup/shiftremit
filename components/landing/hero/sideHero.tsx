1a50fcb4ae315d438a2dbdec723cb92ff60b6f6d
denzubechi <chukwumasamuel371@gmail.com>
2025-11-05 09:55:58 +0100

    edit:ui

--- components/landing/hero/sideHero.tsx
+++ components/landing/hero/sideHero.tsx
@@ -17,30 +17,6 @@ const SideHero = () => {
 
   return (
     <div className="bg-main-dark-II w-[55%] rounded-xl p-6 sm:p-8 text-white shadow-xl">
-      {/* <h2 className="text-center font-semibold font-poppins text-lg mb-4">
-        Get Started
-      </h2> */}
-
-      {/* <div className="w-full flex justify-center my-5">
-        <span className="bg-[#ffffff0d] p-1 inline-flex rounded-md items-center">
-          <p
-            onClick={() => setIsFiat(true)}
-            className={`text-white  text-sm w-[114px] cursor-pointer text-center font-poppins font-medium px-5 py-1.5 rounded-md ${
-              isFiat ? "bg-main" : ""
-            }`}
-          >
-            Fiat
-          </p>
-          <p
-            onClick={() => setIsFiat(false)}
-            className={`text-white  text-sm w-[114px] cursor-pointer text-center font-poppins font-medium px-5 py-1.5 rounded-md ${
-              !isFiat ? "bg-main" : ""
-            }`}
-          >
-            Crypto
-          </p>
-        </span>
-      </div> */}
       <Transfer />
 
       <div className="bg-[#ffffff0d] text-[#cccccc] rounded-lg p-4 mb-6 font-poppins text-sm space-y-2">
@@ -60,19 +36,6 @@ const SideHero = () => {
               )}
               Bank
             </p>
-            {/* <p
-              onClick={() => setIsBank(false)}
-              className={`border relative ${
-                !isBank ? "border-main" : "border-white"
-              } rounded-sm p-1`}
-            >
-              {!isBank && (
-                <span className="bg-main text-white rounded-full inline-flex items-center justify-center w-3.5 h-3.5 absolute -top-2 -right-2">
-                  <IoIosCheckmark size={14} />
-                </span>
-              )}
-              BKash
-            </p> */}
           </div>
         </div>
         <div className="flex font-poppins justify-between">
