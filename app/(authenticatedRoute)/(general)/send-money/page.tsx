e0b2850ebf3f9a442b4f02940944a5708320b8d1
honeybadger <joshviberg3@gmail.com>
2025-11-07 01:38:36 +0000

    feat(ui): adjust send money page responsive margins and text sizes
    Update margins and font sizes in the send money page to improve responsiveness across different screen sizes, aligning with recent UI adjustments.

--- app/(authenticatedRoute)/(general)/send-money/page.tsx
+++ app/(authenticatedRoute)/(general)/send-money/page.tsx
@@ -144,14 +144,14 @@ const SendMoney = () => {
 
   return (
     <SideNav>
-      <div className="my-7 bg-white  rounded-lg mx-auto ">
+      <div className="my-4 md:my-7 bg-white rounded-lg mx-auto ">
         <SendSteps step={1} />
 
-        <div className="my-10">
-          <h1 className=" text-4xl text-[#072032] font-dm-sans text-center mb-3 font-semibold">
+        <div className="mt-5 md:my-10">
+          <h1 className="text-3xl md:text-4xl text-[#072032] font-dm-sans text-center mb-3 font-semibold">
             Send Money
           </h1>
-          <p className="font-poppins text-lg text-[#454745] mb-3 text-center">
+          <p className="font-poppins md:text-lg text-[#454745] mb-3 text-center">
             Fast and reliable international money transfer app.
           </p>
         </div>
