73c1fef73e4b9b8bf8a4e70dc014a336f3b00922
Israel-sanmi <ilesanmiisrael@outlook.com>
2025-11-13 16:23:33 +0100

    updates

--- components/landing/hero/compareRates.tsx
+++ components/landing/hero/compareRates.tsx
@@ -73,7 +73,7 @@ export default function CompareRates({ isOpen, setIsOpen }: any) {
 
     const benchmarkGBP = adminRateData?.benchmarkGBP || 8;
 
-    const lemfiRate = ratesData.lemfi.rate;
+    const lemfiRate = ratesData?.lemfi?.rate || 1903;
 
     const shiftRemitCurrentRate = lemfiRate + benchmarkGBP;
 
@@ -100,21 +100,21 @@ export default function CompareRates({ isOpen, setIsOpen }: any) {
       // },
       {
         ...PROVIDER_MAP["nala"],
-        currentRate: ratesData.nala.rate,
-        discount: baseComparisonRate - ratesData.nala.rate,
-        sortRate: ratesData.nala.rate,
+        currentRate: ratesData?.nala?.rate || 1895,
+        discount: baseComparisonRate - ratesData?.nala?.rate||1895,
+        sortRate: ratesData?.nala?.rate||1895,
       },
       {
         ...PROVIDER_MAP["lemfi"],
-        currentRate: ratesData.lemfi.rate,
-        discount: baseComparisonRate - ratesData.lemfi.rate,
-        sortRate: ratesData.lemfi.rate,
+        currentRate: ratesData?.lemfi?.rate||1903,
+        discount: baseComparisonRate - ratesData?.lemfi?.rate||1903,
+        sortRate: ratesData?.lemfi?.rate||1903,
       },
       {
         ...PROVIDER_MAP["sendApp"],
-        currentRate: ratesData.sendApp.rate,
-        discount: baseComparisonRate - ratesData.sendApp.rate,
-        sortRate: ratesData.sendApp.rate,
+        currentRate: ratesData?.sendApp?.rate||1885 ,
+        discount: baseComparisonRate - ratesData?.sendApp?.rate||1885 ,
+        sortRate: ratesData?.sendApp?.rate||1885,
       },
     ];
 
